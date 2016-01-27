5fac8c62f679772e180d9926fe52bbff0f442d87
njabisetti <njabisetti@aksharasolutions.com>
2016-01-27 18:39:40 +0530

    Initial version.

---
+++ src/guest_base.py
@@ -0,0 +1,85 @@
+from flask import request
+import os
+import requests
+from common.config_manager import discover_service, get_logger
+from common.helper import base64_encode, get_consul_server
+import json
+from validate_email import validate_email
+
+consul_server = get_consul_server()
+logger = get_logger(consul_server)
+
+def signup(data):
+	if "email" not in data:
+		return {"error": True, "msg": "Email not provided"}, 404
+	if not validate_email(data['email']):
+		return {"error": True, "msg": "Supplied value is not an email"}, 404
+
+	if "OrganizationName" not in data:
+		return {"error": True, "msg": "OrganizationName not provided"}, 404
+	
+	#Sign up users are created as System Administrators	
+	ret_msg, status_code = get_service_response("create", {"Email": data["email"], "OrganizationName": data["OrganizationName"], "PermissionLevel":0})
+	if status_code != 201:
+		return ret_msg, status_code
+
+	user_id = ret_msg["EntityID"]
+	user_struct = ret_msg
+	pwd = ret_msg["HashedPassword"]
+	token = base64_encode(ret_msg["Email"] + ":" + pwd)
+	user_struct["AuthToken"] = token
+	# final_header["User-Id"] = user_id	
+
+	return user_struct, 201
+
+def login(data):
+	if not data:
+		return {"error": True, "msg": "Request was not understood"}, 500
+	if not "username" in data:
+		return {"error": True, "msg": "Username/Password is wrong"}, 401
+	if not "password" in data:
+		return {"error": True, "msg": "Username/Password is wrong"}, 401
+		
+	ret_msg, status_code = get_service_response("login", data)
+
+	if "HashedPassword" not in ret_msg:
+		return {}, status_code
+
+	pwd = ret_msg["HashedPassword"]
+	token = base64_encode(data["username"] + ":" + pwd)
+	ret_msg.pop("HashedPassword")
+	ret_msg["AuthToken"] = token
+
+	return ret_msg, 200
+
+def reset_password(data):
+	if "Email" not in data:
+		return {"error": True, "msg": "Cannot perform action. No Email"}, 401
+
+	ret_msg, status_code = get_service_response("resetpassword", data)
+
+	item = {}
+	item["Status"] = ret_msg["Status"]
+
+	return item, status_code
+
+def get_service_response(service_name, data):
+	request_headers = {}
+	for k, v in request.headers.items():
+		request_headers[k] = v
+	send_data = json.dumps(data)
+	final_header = {}
+	final_header["User-Agent"] = request_headers["User-Agent"]
+
+	if "Content-Type" in request_headers:
+		final_header["Content-Type"] = request_headers["Content-Type"]
+	else:
+		final_header["Content-Type"] = "application/json"
+
+	ret = discover_service(consul_server, "user")
+	service_addr= ret[0]["Address"]
+	service_port = ret[0]["ServicePort"]
+	user_service = "http://" + service_addr + ":" + str(service_port) + "/"+service_name
+	r = requests.post(user_service, data=send_data, headers=final_header)
+	
+	return r.json(), r.status_code
---
+++ src/guest_service.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+import os
+
+from common.config_manager import get_logging_handler, get_logger, is_local_or_dev_environment
+from common.helper import get_consul_server
+
+import guest_base
+
+app = Flask(__name__)
+
+consul_server = get_consul_server()
+logger = get_logger(consul_server)
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({"error": True, "msg": "Unknown error occured on server."}), 500
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({"error": True, "msg": "Resource not found."}), 404
+
+@app.route("/signup", methods = ["POST"])
+def signup():
+
+	data = request.get_json(force=False, silent=True)
+	if not data:
+		return jsonify({"error": True, "msg": "Request was not understood"}), 500	
+	
+	user_struct, status_code = guest_base.signup(data)
+
+	return jsonify(user_struct), status_code
+
+
+@app.route("/login", methods = ["POST"])
+def login():
+	
+	data = request.get_json(force=False, silent=True)
+
+	ret_msg, status_code = guest_base.login(data)
+
+	return jsonify(ret_msg), status_code
+
+@app.route("/resetpassword", methods = ["POST"])
+def reset_password():
+	
+	data = request.get_json(force=False, silent=True)
+	if not data:
+		return jsonify({"error": True, "msg": "Request was not understood"}), 500
+	
+	item, status_code = guest_base.reset_password(data)
+
+	return jsonify(item), status_code
+
+if __name__ == "__main__":
+	app.logger.addHandler(get_logging_handler(consul_server))
+	if is_local_or_dev_environment(consul_server):
+		app.run(debug=True,host='0.0.0.0')
+	else:
+		app.run(debug=False,host='0.0.0.0')
